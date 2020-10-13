29d1844647b3935dab3c8b5b952f1c737c5a4391
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-10-13 10:01:14 +0300

    adds express to project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Main page</h1>`);
+});
+
+app.get('/portfolio', (req, res) => {
+  res.send(`<h1>Portfolio</h1>`);
+});
+
+app.listen(5001, () => {
+  console.log(`server running at 5001`);
+});
